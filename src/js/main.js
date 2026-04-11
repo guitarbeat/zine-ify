70e72d297fff966584c25e501b3b0147d8c11d2c
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2026-04-10 23:05:01 -0500

    refactor: update CSS import path to reflect directory reorganization

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import '../styles/index.css';
+import '../css/styles.css';
 import { PDFProcessor } from './pdf-processor.js';
 import { UIManager } from './zine-ui.js';
 import { toast } from './toast.js';
