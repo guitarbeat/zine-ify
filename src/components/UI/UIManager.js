8f1c28cdedaff18daa53deeb51d9d94bfcd1c995
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2026-08-27 08:06:23 -0500

    Batch DOM updates during layout generation (#466)

--- src/components/UI/UIManager.js
+++ src/components/UI/UIManager.js
@@ -656,7 +656,12 @@ export class UIManager {
     this.renderer.render(
       numPages,
       template,
-      { pageNumbersVisible: this.pageNumbersVisible },
+      {
+        pageNumbersVisible: this.pageNumbersVisible,
+        pageImages: paperSettings.pageImages,
+        pageFlips: paperSettings.pageFlips,
+        pageZooms: paperSettings.pageZooms
+      },
       handlers,
       dimensions
     );
